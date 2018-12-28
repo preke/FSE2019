18b951c0d268b2b7ab1e7e770d9f922196e54c55
preke <geek00021@gmail.com>
2018-12-29 04:06:45 +0800

    .

--- src/model.py
+++ src/model.py
@@ -42,3 +42,14 @@ class DecoderRNN(nn.Module):
 
     def initHidden(self):
         return torch.zeros(1, 1, self.hidden_size, device=self.device)
+
+class Seq2Seq(nn.Module):
+    def __init__(self, args):
+        super(Seq2Seq, self).__init__()
+        self.encoder = EncoderRNN(args.input_size, args.hidden_size, args.device)
+        self.decoder = DecoderRNN(args.hidden_size, args.output_size, args.device)
+        self.init_hidden = self.encoder.initHidden()
+    def forward(self, post):
+        _, encoder_rep = self.encoder(post, self.init_hidden)
+
+        pass
